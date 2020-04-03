0000fe0fb659bce819ddb5188ea38f70aab6b62b
Paola Dominguez <paoladomdiaz@gmail.com>
2020-04-02 19:39:02 -0600

    First commit

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.uca.capas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.uca.capas.domain.Usuario;
+
+@Controller
+public class MainController {
+	@GetMapping("/login")
+	public String enviarForm(Usuario user) {
+		return "Login";
+	}
+	
+	@PostMapping("/validacion")
+	public String procesarForm(Usuario user) {
+		if(user.getUsuario().equals("paola") && user.getContrasenia().equals("1234")) {
+			return "MostrarMensajeV";	
+		} else {
+			return "MostrarMensajeF";
+		}
+		
+	}
+}
